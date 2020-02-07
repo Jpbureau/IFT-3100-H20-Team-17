b54bb8ead8fecd4978fe16ecde7a1a1474d4eec5
William Picard <william.picard@hotmail.com>
2020-02-07 10:56:03 -0500

    Add file .png if file extension dosn't exists

--- src/fileManager.cpp
+++ src/fileManager.cpp
@@ -5,10 +5,16 @@ void FileManager::saveImage() const
 	ofFileDialogResult result = ofSystemSaveDialog("render.png", "Image Export");
 
 	if (result.bSuccess) {
+		filesystem::path path = result.getPath();
+
+		if (!path.has_extension()) {
+			path += ".png";
+		}
+
 		ofImage image;
 		image.grabScreen(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
-		image.save(result.getPath());
+		image.save(path);
 
-		ofLog() << "<export image: " << result.getPath() << ">";
+		ofLog() << "<export image: " << path << ">";
 	}
 }
